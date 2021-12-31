07a0d5a4a06ddaf06194a3724958c3b80563116f
IvanSteuerberg <halloichbinivan@gmail.com>
2021-12-31 13:36:00 +0100

    relaciones entre modelos

--- app/models.py
+++ app/models.py
@@ -17,6 +17,16 @@ class Item(db.Model):
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(12), nullable=False, unique=True)
     description = db.Column(db.String(1024), nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f'Item {self.name}'
+
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email_address = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    budget = db.Column(db.Integer, nullable=False, default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
