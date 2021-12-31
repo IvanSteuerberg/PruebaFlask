0fb4aad51104eeb24de9f4e694b67bce67ec1804
IvanSteuerberg <halloichbinivan@gmail.com>
2021-12-31 09:16:01 +0100

    Pagina home con barra de navegación

--- app.py
+++ app.py
@@ -61,5 +61,10 @@ def update(id):
         return render_template('update.html', task=task)
 
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
