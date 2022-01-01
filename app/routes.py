81cec3d98cfd4719d1bcc3f782198d9013c25084
IvanSteuerberg <halloichbinivan@gmail.com>
2022-01-01 10:20:39 +0100

    Implementado logout de usuario

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask_login import login_user
+from flask_login import login_user, logout_user
 from app import app, db
 from flask import render_template, request, redirect, url_for, flash
 from app.models import Todo, Item, User
@@ -90,3 +90,10 @@ def login_page():
         else:
             flash('Wrong password! Please try again', category='danger')
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have logged out!", category='info')
+    return redirect(url_for('home_page'))
