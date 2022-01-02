93547227f428475fee4fe1fa4d4dbbc1041f2fd0
IvanSteuerberg <halloichbinivan@gmail.com>
2022-01-02 08:25:57 +0100

    Comprar items

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from app import app, db
 from flask import render_template, request, redirect, url_for, flash
 from app.models import Todo, Item, User
-from app.forms import RegisterForm, LoginForm
-from flask_login import login_user, logout_user, login_required
+from app.forms import RegisterForm, LoginForm, PurchaseItemForm
+from flask_login import login_user, logout_user, login_required, current_user
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -55,11 +55,24 @@ def home_page():
     return render_template('home.html')
 
 
-@app.route('/market')
+@app.route('/market', methods=['GET', 'POST'])
 @login_required
 def market_page():
-    items = Item.query.all()
-    return render_template('market.html', items=items)
+    purchase_form = PurchaseItemForm()
+    if request.method == "POST":
+        purchased_item = request.form.get('purchased_item')
+        p_item_object = Item.query.filter_by(name=purchased_item).first()
+        if p_item_object:
+            if current_user.can_purchase(p_item_object):
+                p_item_object.buy(current_user)
+                flash(f"Congratulations! You purchased {p_item_object.name} for {p_item_object.price}€",
+                      category='success')
+            else:
+                flash(f"Unfortunately, you don't have enough money to purchase {p_item_object.name}", category='danger')
+        return redirect(url_for('market_page'))
+    if request.method == "GET":
+        items = Item.query.filter_by(owner=None)
+        return render_template('market.html', items=items, purchase_form=purchase_form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
